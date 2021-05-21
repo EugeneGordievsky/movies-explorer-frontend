330396725dca16f45cedcb9ea30c0f3031001e08
Eugene Gordievsky <eugene.gordievsky@yandex.ru>
2021-05-21 19:21:45 +0300

    feat: logo click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,25 @@
 import logo from '../../images/logo.svg';
-import { Route } from 'react-router-dom';
+import { NavLink, Route, useHistory } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 
 export default function Header(props) {
+  const history = useHistory();
+
+  const backToMain = () => {
+    history.push('./')
+  };
+
   return (
     <>
       <Route exact path='/'>
         <header className='header header_main'>
-          <img src={logo} alt='Логотип Movie-Explorer' className='header__logo'/>
+          <img src={logo} alt='Логотип Movie-Explorer' className='header__logo' onClick={backToMain} />
           <Navigation setMobileNav={props.setMobileNav} loggedIn={props.loggedIn} />
         </header>
       </Route>
       <Route exact path={['/profile', '/movies', '/saved-movies']}>
         <header className='header'>
-          <img src={logo} alt='Логотип Movie-Explorer' className='header__logo' />
+          <img src={logo} alt='Логотип Movie-Explorer' className='header__logo' onClick={backToMain} />
           <Navigation setMobileNav={props.setMobileNav} loggedIn={props.loggedIn} />
         </header>
       </Route>
